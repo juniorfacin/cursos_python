5231927d56d6a88a40c0e8981de26c1871e3bc4a
Junior Facin <jufacin@hotmail.com>
2024-04-27 16:47:05 -0300

    Início do Curso:
    Python: crie sua primeira aplicação

---
+++ sabor-express/app.py
@@ -0,0 +1,11 @@
+print("""
+      𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤 \n
+      """)
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
